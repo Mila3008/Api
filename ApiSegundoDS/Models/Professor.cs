f7ac6ce19bf16b8a5b2ce0bf0bfc8e6432f7c825
Mila3008 <camilapassosdafiuza@gmail.com>
2025-03-10 09:07:15 -0300

    feat: Classes Escola, Aluno e Professor

---
+++ ApiSegundoDS/Models/Professor.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiSegundoDS.Models
+{
+    public class Professor
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public Escola Escola { get; set; }
+        public Aluno Aluno { get; set; }
+    }
+}
